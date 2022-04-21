5b99ec9ce6badacbf0ef4855ce5a7f186ab8232d
Vitor Melo <vitor.vmelo@tdx.com.br>
2022-04-20 22:29:53 -0300

    Melhorias no card de perfil

--- lib/components/profile_card.dart
+++ lib/components/profile_card.dart
@@ -1,7 +1,10 @@
+import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:flutter/material.dart';
 
 import '../../libraries/responsive.dart';
+import '../bloc/login_bloc.dart';
 import '../libraries/constants.dart';
+import '../models/usuario.dart';
 
 class ProfileCard extends StatelessWidget {
   const ProfileCard({
@@ -10,41 +13,51 @@ class ProfileCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin:  const EdgeInsets.only(left: Constants.defaultPadding),
-      padding: const EdgeInsets.symmetric(
-        horizontal: Constants.defaultPadding,
-        vertical: Constants.defaultPadding / 2,
-      ),
-      decoration: BoxDecoration(
-        color: Constants.secondary,
-        borderRadius: const BorderRadius.all(Radius.circular(10)),
-        border: Border.all(color: Colors.white10),
-      ),
-      child: Row(
-        children: [
-          Container(
-              height: 38,
-              width: 38,
-              decoration: BoxDecoration(
-                //image: imageProfile!,
-                image: Constants.imagemProfileDecorator,
-                borderRadius: BorderRadius.circular(50),
-              )),
-          if (!Responsive.isMobile(context))
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: Constants.defaultPadding / 2),
-              child: Text(
-                "Leonardo Jose",
-                style: TextStyle(color: Colors.white),
-              ),
+    return FutureBuilder<Usuario>(
+        future: BlocProvider.getBloc<LoginBloc>().checkLogged(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return Container();
+          }
+          Usuario _usuario = snapshot.data!;
+
+          return Container(
+            margin: const EdgeInsets.only(left: Constants.defaultPadding),
+            padding: const EdgeInsets.symmetric(
+              horizontal: Constants.defaultPadding,
+              vertical: Constants.defaultPadding / 2,
             ),
-          const Icon(
-            Icons.keyboard_arrow_down,
-            color: Colors.white,
-          ),
-        ],
-      ),
-    );
+            decoration: BoxDecoration(
+              color: Constants.secondary,
+              borderRadius: const BorderRadius.all(Radius.circular(10)),
+              border: Border.all(color: Colors.white10),
+            ),
+            child: Row(
+              children: [
+                Container(
+                    height: 38,
+                    width: 38,
+                    decoration: BoxDecoration(
+                      //image: imageProfile!,
+                      image: Constants.imagemProfileDecorator,
+                      borderRadius: BorderRadius.circular(50),
+                    )),
+                if (!Responsive.isMobile(context))
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: Constants.defaultPadding / 2),
+                    child: Text(
+                      _usuario.nome,
+                      style: const TextStyle(color: Colors.white),
+                    ),
+                  ),
+                const Icon(
+                  Icons.keyboard_arrow_down,
+                  color: Colors.white,
+                ),
+              ],
+            ),
+          );
+        });
   }
 }
